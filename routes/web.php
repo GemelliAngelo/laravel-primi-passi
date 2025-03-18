3371f7a496bb301ad07e3ce8bde39e8e852b0a1f
GemelliAngelo <gemeangelo27@gmail.com>
2025-03-18 17:16:23 +0100

    add bonus route function

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,25 @@ Route::get('/', function () {
     $dati = "Homepage";
 
     return view('home',["dati" => $dati]);
-});
+})->name('home');
 
 Route::get('/about', function () {
 
     $dati = "About";
 
     return view('about',["dati" => $dati]);
-});
+})->name('about');
 
 Route::get('/contacts', function () {
 
     $dati = "Contacts";
 
     return view('contacts',["dati" => $dati]);
-});
+})->name('contacts');
 
 Route::get('/service', function () {
 
     $dati = "Service";
 
     return view('service',["dati" => $dati]);
-});
+})->name('service');
